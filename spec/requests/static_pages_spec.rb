ee7f84d08aa89ed17c84601f182fa16b77752fb4
tdash <tdash_kofate@live.com>
2014-02-13 06:05:51 -0800

    Finished Static Pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  
+  # Testing Content for Home Page
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+    
+  # Testing Title for Home Page
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+
+  # Testing Content for Help Page
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+  # Testing Title for Help Page
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+
+  # Testing Content for About Page
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+  # Testing Title for About Page
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+
+end
